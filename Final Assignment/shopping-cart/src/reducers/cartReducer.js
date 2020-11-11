9162d1d1cdf761a6b519cdd2a8cf5ef3c54f1895
Poojana <Poojana.V@ibm.com>
2020-11-11 13:44:14 +0530

    changes

--- Final Assignment/shopping-cart/src/reducers/cartReducer.js
+++ Final Assignment/shopping-cart/src/reducers/cartReducer.js
@@ -1,17 +1,7 @@
 function cartReducer (currentState = [], action) {
     if (action.type === 'ADD_PRODUCT_TO_CART') {
         let newState = [];
-        let found = false;
-            newState = currentState.map((product) => {
-                if (action.payload.productID === product.productID){
-                    found = true;
-                    return { ...product , quantity: action.payload.quantity};
-                }
-                return product;
-            });
-            if (!found){
-                newState.push(action.payload);
-            };
+        newState = [...currentState, action.payload]
         return newState;
     }
     return currentState;
